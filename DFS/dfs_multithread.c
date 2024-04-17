b84a261dd23b90b9856a1cabeaa010cf203f3597
Vicente Sampaio <141889498+VicenteSam@users.noreply.github.com>
2024-04-17 19:08:59 -0400

    refactoring dfs_multithread.c

--- DFS/dfs_multithread.c
+++ DFS/dfs_multithread.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
 #include "stack.h"
 
 #define MAX 10
@@ -23,14 +25,14 @@ bool visited[MAX] = {false};
 pthread_t threads[NUM_THREADS];
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-void printGraph() {
+void print_graph() {
     printf("   ");
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < MAX; i++)
         printf("%3d", i);
     printf("\n");
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < MAX; i++) {
         printf("%3d", i);
-        for (int j = 0; j < 10; j++)
+        for (int j = 0; j < MAX; j++)
             printf("%3ld", graph[i][j]);
         printf("\n");
     }
@@ -40,36 +42,32 @@ void* dfs(void* arg) {
     long thread_id = (long)arg;
     Stack* stack = createStack();
 
-    int start = thread_id * (10 / NUM_THREADS);
-    int end = (thread_id + 1) * (10 / NUM_THREADS);
+    int start = thread_id * (MAX / NUM_THREADS);
+    int end = (thread_id + 1) * (MAX / NUM_THREADS);
     if (thread_id == NUM_THREADS - 1) {
-        end = 10;
+        end = MAX;
     }
 
     for (int i = start; i < end; i++) {
+        pthread_mutex_lock(&mutex);
         if (!visited[i]) {
-            pthread_mutex_lock(&mutex);
-            if (!visited[i]) {
-                push(stack, i);
-                visited[i] = true;
-                pthread_mutex_unlock(&mutex);
+            push(stack, i);
+            visited[i] = true;
+        }
+        pthread_mutex_unlock(&mutex);
 
-                while (!stackIsEmpty(stack)) {
-                    int node = pop(stack);
-                    printf("Thread %ld is visiting node: %d\n", thread_id, node);
-                    for (int j = 0; j < 10; j++) {
-                        if (graph[node][j] != 0 && !visited[j]) {
-                            pthread_mutex_lock(&mutex);
-                            if (!visited[j]) {
-                                push(stack, j);
-                                visited[j] = true;
-                            }
-                            pthread_mutex_unlock(&mutex);
-                        }
+        while (!stackIsEmpty(stack)) {
+            int node = pop(stack);
+            printf("Thread %ld is visiting node: %d\n", thread_id, node);
+            for (int j = 0; j < MAX; j++) {
+                if (graph[node][j] != 0 && !visited[j]) {
+                    pthread_mutex_lock(&mutex);
+                    if (!visited[j]) {
+                        push(stack, j);
+                        visited[j] = true;
                     }
+                    pthread_mutex_unlock(&mutex);
                 }
-            } else {
-                pthread_mutex_unlock(&mutex);
             }
         }
     }
@@ -77,8 +75,9 @@ void* dfs(void* arg) {
     destroyStack(&stack);
 }
 
+
 int main() {
-    printGraph();
+    print_graph();
     clock_t start = clock();
 
     printf("DFS:\n");

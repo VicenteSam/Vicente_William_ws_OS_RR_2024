3621d887a7e6541700ef79687624d11d7df9573f
Vicente Sampaio <141889498+VicenteSam@users.noreply.github.com>
2024-04-16 21:50:09 -0400

    using a static matrix[10][10]

--- DFS/dfs.c
+++ DFS/dfs.c
@@ -2,34 +2,31 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define MAX 20
+#define MAX 10
 
-long graph[MAX][MAX];
+long graph[MAX][MAX] = {
+    {0, 1, 1, 0, 0, 0, 0, 0, 0, 0},
+    {1, 0, 1, 1, 0, 0, 0, 0, 0, 0},
+    {1, 1, 0, 0, 1, 0, 0, 0, 0, 0},
+    {0, 1, 0, 0, 0, 1, 0, 0, 0, 0},
+    {0, 0, 1, 0, 0, 1, 1, 0, 0, 0},
+    {0, 0, 0, 1, 1, 0, 0, 1, 0, 0},
+    {0, 0, 0, 0, 1, 0, 0, 0, 1, 0},
+    {0, 0, 0, 0, 0, 1, 0, 0, 1, 1},
+    {0, 0, 0, 0, 0, 0, 1, 1, 0, 1},
+    {0, 0, 0, 0, 0, 0, 0, 1, 1, 0}
+};
 long visited[MAX];
 int cont = 0;
 
-void createGraph() {
-    srand(time(NULL));
-    for (int i = 1; i <= MAX; i++) {
-        for (int j = 1; j <= MAX; j++) {
-            if (i != j) {
-                graph[i][j] = rand() % 2;
-                graph[j][i] = graph[i][j];
-            } else {
-                graph[i][j] = 0;
-            }
-        }
-    }
-}
-
 void printGraph() {
     printf("   ");
-    for (int i = 1; i <= MAX; i++)
-        printf("%3d", i);
+    for (int i = 0; i < MAX; i++)
+        printf("%3d", i + 1);
     printf("\n");
-    for (int i = 1; i <= MAX; i++) {
-        printf("%3d", i);
-        for (int j = 1; j <= MAX; j++)
+    for (int i = 0; i < MAX; i++) {
+        printf("%3d", i + 1);
+        for (int j = 0; j < MAX; j++)
             printf("%3ld", graph[i][j]);
         printf("\n");
     }
@@ -37,10 +34,10 @@ void printGraph() {
 }
 
 void dfs_aux(int vertex) {
-    printf("-> %d ", vertex);
+    printf("-> %d ", vertex + 1);
     visited[vertex] = ++cont;
         
-    for (int j = 1; j <= MAX; j++) {
+    for (int j = 0; j < MAX; j++) {
         if (graph[vertex][j] == 1 && visited[j] == 0)
             dfs_aux(j);
     }
@@ -49,20 +46,18 @@ void dfs_aux(int vertex) {
 void dfs(int orig) {
     cont = 0;
     
-    for (int j = 1; j <= MAX; j++)
+    for (int j = 0; j < MAX; j++)
         visited[j] = 0;
     dfs_aux(orig);
     printf("\n");
-    for(int j=1; j <= MAX; j++)
+    for(int j = 0; j < MAX; j++)
         printf("| %ld |", visited[j]);
-    
 }
 
 int main() {
-    createGraph();
     printGraph();
     clock_t start = clock();
-    dfs(1);
+    dfs(0);
     clock_t end = clock();
     double execution_time = ((double)(end - start)) / CLOCKS_PER_SEC;
     printf("\nRunTime: %.6f seconds\n", execution_time);
